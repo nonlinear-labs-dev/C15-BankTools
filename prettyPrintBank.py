9397f78254775fb3a26b03ed40864579fac6460d
schlenkibus <justus.scheil@parteimail.de>
2018-08-02 16:34:53 +0200

    added another example of usage

---
+++ prettyPrintBank.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding=UTF8
+import sys
+import NonlinearLabsBankTools
+
+def prettyPrintBank(filename):
+    parser = NonlinearLabsBankTools.NLParser(filename)
+    bankNum = 0
+    for bank in parser.getBanks():
+        bankNum += 1
+        print('\t%i - %s' % (bankNum, bank.getNodeValue("name")))
+        print("-"*len(bank.getNodeValue("name"))*(3))
+        presetNum = 0
+        for preset in bank.getPresets():
+            presetNum += 1
+            print('%s\t %i\t %s' % (preset.getNodeValue("color"), presetNum, preset.getNodeValue("name")))
+#end printAllTags
+
+#main
+if len(sys.argv) < 2:
+    print("use: ./prettyPrintBank.py <BANK>.xml")
+    exit(1)
+
+inFile = sys.argv[1]
+prettyPrintBank(inFile)
+#endmain
