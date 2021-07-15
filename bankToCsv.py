80d0f4236abc49001a23ba2be5e88be58d020f94
schlenkibus <justus.scheil@parteimail.de>
2021-07-15 12:29:21 +0200

    added CSV headers, mc names + mc mappings

--- bankToCsv.py
+++ bankToCsv.py
@@ -5,6 +5,8 @@ import NonlinearLabsBankTools
 
 def parseAsCSV(filename):
     lines = list()
+    lines.append("NR,Name,UUID,Color,Comment,MC A Name,MC A Target,MC B Name,MC B Target,MC C Name,MC C Target,MC D Name,MC D Target,MC E Name,MC E Target,MC F Name,MC F Target\n")
+    
     parser = NonlinearLabsBankTools.NLParser(filename)                          #Startet den Lesevorgang, bietet eingelesene Bänke mit 'getBanks()' an
     for bank in parser.getBanks():                                              #Schleife über alle Bänke des eingelesenen XML's
         numPresets = 0
@@ -17,36 +19,91 @@ def parseAsCSV(filename):
             mc5 = ""
             mc6 = ""
 
-
             mcAssignments = [[], [], [], [], [], []]
 
             for param in preset.getParameters():
                 modSrc = param.getNodeValue("modSrc")
-                if modSrc != "None":
+                if modSrc != "None" and int(modSrc) != 0:
                     mcAssignments[int(modSrc) - 1].append(param.getNodeValue("id"))                    
 
             for param in preset.getParameters():
-                if param.getNodeValue("id") == "243":
-                    mc1 = param.getNodeValue("givenName")
-                if param.getNodeValue("id") == "244":
-                    mc2 = param.getNodeValue("givenName")
-                if param.getNodeValue("id") == "245":
-                    mc3 = param.getNodeValue("givenName")
-                if param.getNodeValue("id") == "246":
-                    mc4 = param.getNodeValue("givenName")
-                if param.getNodeValue("id") == "369":
-                    mc5 = param.getNodeValue("givenName")
-                if param.getNodeValue("id") == "371":
-                    mc6 = param.getNodeValue("givenName")
-                
-                                                                                            #Formatierte Ausgabe der für den User relevanten Daten
+                idSplit = param.getNodeValue("id").split("-")
+                parameterNumber = idSplit[1]
 
-            lines.append("%i,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (numPresets,                     #%i - Integer, %s - String
-            preset.getNodeValue("name"),
-            preset.getNodeValue("uuid"),
-            preset.getNodeValue("color"),
-            preset.getNodeValue("Comment"),
-            mc1, mc2, mc3, mc4, mc5, mc6))
+                if parameterNumber == "243":
+                    mc1 = param.getNodeValue("givenName")
+                    if mc1 == "None" or mc1 == "":
+                        mc1 = "MC A"
+                elif parameterNumber == "244":
+                    mc2 = param.getNodeValue("givenName")
+                    if mc2 == "None" or mc2 == "":
+                        mc2 = "MC B"
+                elif parameterNumber == "245":
+                    mc3 = param.getNodeValue("givenName")
+                    if mc3 == "None" or mc3 == "":
+                        mc3 = "MC C"
+                elif parameterNumber == "246":
+                    mc4 = param.getNodeValue("givenName")
+                    if mc4 == "None" or mc4 == "":
+                        mc4 = "MC D"
+                elif parameterNumber == "369":
+                    mc5 = param.getNodeValue("givenName")
+                    if mc5 == "None" or mc5 == "":
+                        mc5 = "MC E"
+                elif parameterNumber == "371":
+                    mc6 = param.getNodeValue("givenName")
+                    if mc6 == "None" or mc6 == "":
+                        mc6 = "MC F"
+                
+            mc1Targets = ""
+            mc2Targets = ""
+            mc3Targets = ""
+            mc4Targets = ""
+            mc5Targets = ""
+            mc6Targets = ""
+
+            index = 0
+            for assignments in mcAssignments:
+                if assignments:
+                    string = ' '.join(str(e) for e in assignments)
+                    if index == 0:
+                        mc1Targets = string
+                    elif index == 1:
+                        mc2Targets = string
+                    elif index == 2:
+                        mc3Targets = string
+                    elif index == 3:
+                        mc4Targets = string
+                    elif index == 4:
+                        mc5Targets = string
+                    elif index == 5:
+                        mc6Targets = string
+                index += 1
+                                                                                            
+
+            rawComment = preset.getNodeValue("Comment")
+            for c in [',', ';']:
+                rawComment = rawComment.replace(c, ' ')
+            cleanComment = rawComment
+
+            #Formatierte Ausgabe der für den User relevanten Daten
+            #%i - Integer, %s - String
+
+            lines.append("%i,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % 
+                (
+                    numPresets,
+                    preset.getNodeValue("name"),
+                    preset.getNodeValue("uuid"),
+                    preset.getNodeValue("color"),
+                    cleanComment,
+                    mc1, mc1Targets, 
+                    mc2, mc2Targets, 
+                    mc3, mc3Targets, 
+                    mc4, mc4Targets, 
+                    mc5, mc5Targets, 
+                    mc6, mc6Targets
+                )
+            )
     return lines
 #end parseAsCSV
 
@@ -55,9 +112,8 @@ def writeListOfLinesToFile(file, lines):
     for line in lines:
         file.write(line)
     file.close()
-#end writeListOfLinesToFile
 
-                                                                                #Programmeinstieg
+
 #main
 if len(sys.argv) < 2:
     print("use: ./bankToCsv <BANK>.xml <OUT>.csv")
@@ -71,4 +127,3 @@ if len(sys.argv) >= 3:
 
 lines = parseAsCSV(inFile)
 writeListOfLinesToFile(outFile, lines)
-#endmain
