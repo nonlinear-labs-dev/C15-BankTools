bd0ea16e498028ce79e51b06997ac34ac7058740
schlenkibus <justus.scheil@parteimail.de>
2023-07-19 09:30:03 +0200

    fix continue checking even if difference was already found. And created an IDs.py file (generated by configuration and copied into this repo)

--- doBanksDiffer.py
+++ doBanksDiffer.py
@@ -2,13 +2,24 @@
 # coding=UTF8
 import sys
 import lib.NonlinearLabsBankTools as NonlinearLabsBankTools
+import lib.ids
+
+def readable(id: str) -> str:
+    if id.find("-") != -1:
+        number = id.split("-")[1]
+        return str(lib.ids.ParameterNumber(int(number)))
+    else:
+        return str(lib.ids.ParameterNumber(int(id)))
 
 def banksDiffer(bank1: NonlinearLabsBankTools.NLParser, bank2: NonlinearLabsBankTools.NLParser) -> bool:
+    hadDiff = False
+
     banks1 = bank1.getBanks()
     banks2 = bank2.getBanks()
 
     if len(banks1) != 1 or len(banks2) != 1:
         print(f"banks have different sizes: {len(banks1)} != {len(banks2)}")
+        hadDiff |= True
         return True
 
     banks1 = banks1[0]
@@ -16,7 +27,8 @@ def banksDiffer(bank1: NonlinearLabsBankTools.NLParser, bank2: NonlinearLabsBank
 
     if banks1.getVersion() != banks2.getVersion():
         print(f"banks have different versions: {banks1.getVersion()} != {banks2.getVersion()}")
-
+        hadDiff |= True
+        
     print(f"banks1: {banks1.getName()}")
     print(f"banks2: {banks2.getName()}")
 
@@ -27,49 +39,51 @@ def banksDiffer(bank1: NonlinearLabsBankTools.NLParser, bank2: NonlinearLabsBank
 
         if otherPreset == None:
             print(f"{banks1.getName()}-{preset.getName()} not found in {banks2.getName()}")
+            hadDiff |= True
             return True
 
         for parameter in preset.getParameters():
-            if parameter == None:
-                print(f"Parameter not found in {preset.getName()}")
-
             otherParameter = otherPreset.findParameter(parameter.getID())
 
             if otherParameter == None:
                 print(f"{banks1.getName()}: {preset.getName()} Parameter with ID: {parameter.getID()} not found in {otherPreset.getName()}")
-                return True
-            
-            myKeyCount = len(parameter.getKeys())
-            otherKeyCount = len(otherParameter.getKeys())
+                hadDiff |= True
+                continue
 
-            if myKeyCount != otherKeyCount:
-                print(f"{banks1.getName()}: {preset.getName()} Parameter-Keys differ: {myKeyCount} != {otherKeyCount}. This is most likely a redefinition of a parameter (Modulate/Unmodulateable).")
-                myKeys = " ".join(parameter.getKeys())
-                otherKeys  = " ".join(otherParameter.getKeys())
-                print(f"keys for parameter of the first argument: {myKeys}")
-                print(f"keys for parameter of the second argument: {otherKeys}")
-                return True
+            doKeyCheck = False
 
+            if doKeyCheck:
+                myKeyCount = len(parameter.getKeys())
+                otherKeyCount = len(otherParameter.getKeys())
+
+                if myKeyCount != otherKeyCount:
+                    print(f"{banks1.getName()}: {preset.getName()} Parameter-Keys for {parameter.getID()} differ: {myKeyCount} != {otherKeyCount}. This is most likely a redefinition of a parameter (Modulate/Unmodulateable).")
+                    myKeys = " ".join(parameter.getKeys())
+                    otherKeys  = " ".join(otherParameter.getKeys())
+                    print(f"keys for parameter of the first argument: {myKeys}")
+                    print(f"keys for parameter of the second argument: {otherKeys}")
+                    hadDiff |= True
+        
             myValue = parameter.getNodeValue("value")
             otherValue = otherParameter.getNodeValue("value")
             
             if myValue != otherValue:
-                print(f"{preset.getName()}: {parameter.getID()} Parameter-Value: {myValue} != {otherValue}")
-                return True
-
+                print(f"{preset.getName()}: {readable(parameter.getID())} Parameter-Value: {myValue} != {otherValue}")
+                hadDiff |= True
+        
             if otherParameter.getNodeValue("pedalMode") != parameter.getNodeValue("pedalMode"):
                 print(f"{preset.getName()}: {parameter.getID()} Pedal-Mode: {parameter.getNodeValue('pedalMode')} != {otherParameter.getNodeValue('pedalMode')}")
-                return True
-            
+                hadDiff |= True
+        
             if otherParameter.getNodeValue("modSrc") != parameter.getNodeValue("modSrc"):
                 print(f"{preset.getName()}: {parameter.getID()} mod-source: {parameter.getNodeValue('modSrc')} != {otherParameter.getNodeValue('modSrc')}")
-                return True
-            
+                hadDiff |= True
+        
             if otherParameter.getNodeValue("modAmount") != parameter.getNodeValue("modAmount"):
                 print(f"{preset.getName()}: {parameter.getID()} modulation-amount: {parameter.getNodeValue('modAmount')} != {otherParameter.getNodeValue('modAmount')}")
-                return True
-
-    return False
+                hadDiff |= True
+        
+    return hadDiff
 
 if len(sys.argv) < 2:
     print("use: ./doBanksDiffer.py bank1 bank2")
